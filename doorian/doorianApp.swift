eee071d449e9dfa47bef1251d84572c8e151d109
iceschp <warunya.ks@gmail.com>
2023-01-05 13:05:11 +0700

    Initial Commit

---
+++ doorian/doorianApp.swift
@@ -0,0 +1,17 @@
+//
+//  doorianApp.swift
+//  doorian
+//
+//  Created by Warunya on 5/1/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct doorianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
