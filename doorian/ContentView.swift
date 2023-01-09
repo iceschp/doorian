2c0489ff087340c7e01676471d41c897509f21e8
iceschp <warunya.ks@gmail.com>
2023-01-09 19:55:20 +0700

    add custom tab bar

--- doorian/ContentView.swift
+++ doorian/ContentView.swift
@@ -8,10 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+    @State private var tabSelected: Tab = .house
+        
+        init() {
+            UITabBar.appearance().isHidden = true
+        }
+        
+        var body: some View {
+            ZStack {
+                VStack {
+                    TabView(selection: $tabSelected) {
+                        ForEach(Tab.allCases, id: \.rawValue) { tab in
+                            HStack {
+//                                Image(systemName: tab.rawValue)
+//                                Text("\(tab.rawValue.capitalized)")
+                                Text("หน้า Home")
+                                    .bold()
+                                    
+                                    .animation(nil, value: tabSelected)
+                            }
+                            .tag(tab)
+                            
+                        }
+                    }
+                }
+                VStack {
+                    Spacer()
+                    CustomTabBar(selectedTab: $tabSelected)
+                }
+            }
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
