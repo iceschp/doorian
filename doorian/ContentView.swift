200a5eb3ca16112afc40e51fe2ef4beaff75ad26
iceschp <warunya.ks@gmail.com>
2023-02-06 16:56:26 +0700

    add custobtabbar view

--- doorian/ContentView.swift
+++ doorian/ContentView.swift
@@ -8,87 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+//    @State var selectedTab: Tabs = .home
+    
     var body: some View {
-        CustomTabBarView()
-    }
-}
-
-struct TabLabel : View {
-    var text : String
-    var imageName : String
-    var color : Color
-    
-    var body : some View {
         VStack {
-            Image(systemName: imageName)
-                .resizable()
-                .frame(width: 30, height: 30)
-                .padding(.bottom, 2)
-            Text(text).font(.system(size: 10))
-        }.foregroundColor(color)
-    }
-}
-
-struct TabButton : View {
-    @Binding var currentSelection : Int
-    
-    var selectionIndex : Int
-    var label : TabLabel
-    
-    var body : some View {
-        Button(action: {
-            self.currentSelection = self.selectionIndex
-        })
-        {
-            label
+            CustomTabbar()
         }
-        .foregroundColor(selectionIndex == currentSelection ? Color("dark-green") : Color("red"))
-    }
-}
-
-struct CustomTabBarView : View {
-    @State var currentSelection : Int = 1
-    
-    var body : some View {
-        let house = TabLabel(text: "หน้าแรก", imageName: "house.fill", color: Color("grey-icon"))
-        let viewfinder = TabLabel(text: "", imageName: "viewfinder", color: Color("grey-icon"))
-        let person = TabLabel(text: "โปรไฟล์", imageName: "person.fill", color: Color("grey-icon"))
-        
-        let button1 = TabButton(currentSelection: $currentSelection, selectionIndex: 1, label: house)
-        let button2 = TabButton(currentSelection: $currentSelection, selectionIndex: 2, label: viewfinder)
-        let button3 = TabButton(currentSelection: $currentSelection, selectionIndex: 3, label: person)
-        
-        
-        return VStack() {
-            
-            Spacer()
-            
-            if currentSelection == 1 {
-                HomeView()
-            }
-            else if currentSelection == 2 {
-                ScanningView()
-            }
-            else if currentSelection == 3 {
-                ProfileView()
-            }
-            
-            Spacer()
-            
-            HStack() {
-                button1
-                Spacer()
-                button2
-                Spacer()
-                button3
-                
-            }
-            .padding(.horizontal, 62)
-            .padding(.top, 20)
-            .frame(height: 60)
-            .background(Color("grey-light"))
-        }
-        
+//        VStack {
+//            Spacer()
+//            CustomTabbar(selectedTab: $selectedTab)
+//        }
     }
 }
 
