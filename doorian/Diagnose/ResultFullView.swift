3ca4dc6749916f5dfdcfec3aba7e92022f589158
iceschp <warunya.ks@gmail.com>
2023-02-14 17:55:52 +0700

    add section

--- doorian/Diagnose/ResultFullView.swift
+++ doorian/Diagnose/ResultFullView.swift
@@ -9,22 +9,29 @@ import SwiftUI
 
 struct ResultFullView: View {
     var body: some View {
-        VStack {
-            HeaderDisease()
-            
-            VStack(alignment: .leading) {
-                Image("durian_image")
-                    .resizable()
-                    .frame(maxWidth: 360, maxHeight: 160)
-                    .cornerRadius(20)
-                    .padding(.bottom, 29)
-                
-                SciSection()
-                GeneralDetail()
-                
+        NavigationView {
+            ScrollView (.vertical, showsIndicators: false) {
+                VStack {
+                    HeaderDisease()
+                    
+                    VStack(alignment: .leading) {
+                        Image("durian_image")
+                            .resizable()
+                            .frame(maxWidth: 360, maxHeight: 160)
+                            .cornerRadius(20)
+                            .padding(.bottom, 29)
+                        
+                        SciSection()
+                        GeneralDetail()
+                        FullDetail()
+                        HowToCure()
+                        Instruction()
+                        
+                    }
+                }
+                .frame(maxWidth: 360)
             }
         }
-        .frame(maxWidth: 360)
     }
 }
 struct HeaderDisease: View {
@@ -85,6 +92,113 @@ struct GeneralDetail: View {
             }
             .padding(10)
         }
+        .frame(maxWidth: 360)
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(Color("grey-light"), lineWidth: 1)
+        )
+    }
+}
+
+struct FullDetail: View {
+    var body: some View {
+        HStack(alignment: .top) {
+            Group {
+                Image(systemName: "magnifyingglass")
+                    .foregroundColor(Color("A3B694"))
+                    .padding(.trailing, 8)
+                
+                VStack(alignment: .leading) {
+                    Text("อาการของโรคใบไหม้")
+                        .font(.system(size: 14))
+                        .bold()
+                        .foregroundColor(Color("dark-green"))
+                        .padding(.bottom, 4)
+                    
+                    Text("- เริ่มแรกจะพบบนใบมีแผลคล้ายถูกน้ำร้อนลวก \n - ต่อมาแผลจะขยายตัวและเปลี่ยนเป็นสีน้ำตาล มีขนาดและ รูปร่างไม่แน่นอน จากนั้นจะลุกลามไปยังใบปกติข้างเคียง \n - กรณีที่มีความชื้นสูง เชื้อราดังกล่าวจะสร้างเส้นใยคล้ายใย แมงมุมยึดใบให้ติดกัน ใบที่เป็นโรคจะไหม้ แห้งหลุดร่วงไปสัมผัส กับใบด้านล่าง ทำให้โรคระบาดลุกลามจนเห็นใบไหม้เกิดเป็นหย่อมๆ \n - ใบจะร่วงจนเหลือแต่กิ่ง และกิ่งแห้งในที่สุด ทำให้ต้นทุเรียน เสียรูปทรง")
+                        .font(.system(size: 14))
+                }
+            }
+            .padding(10)
+        }
+        .frame(maxWidth: 360)
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(Color("grey-light"), lineWidth: 1)
+        )
+    }
+}
+
+struct HowToCure: View {
+    var body: some View {
+        HStack(alignment: .top) {
+            Group {
+                Image(systemName: "wand.and.stars")
+                    .foregroundColor(Color("A3B694"))
+                    .padding(.trailing, 8)
+                
+                VStack(alignment: .leading) {
+                    Text("วิธีการแก้ไข")
+                        .bold()
+                        .foregroundColor(Color("dark-green"))
+                        .padding(.bottom, 4)
+                    
+                    Text("ฉีดพ่นสารกำจัดเชื้อราที่ใบให้ทั่วทั้งต้นด้วย")
+                    
+                    HStack {
+                        Image(systemName: "hand.thumbsup.fill")
+                            .foregroundColor(Color("FFC700"))
+                        Text("Recommend: เฮกซะโคนาโซล 5% SC")
+                    }
+                    .frame(maxWidth: 300, minHeight: 27)
+                    .cornerRadius(10)
+                    .background(Color("FEFCCB"))
+                    
+                    Text("- คอปเปอร์ไฮดรอกไซด์ 77% WP \n- คอปเปอร์ออกซีคลอไรด์ 85% WP\n- คอปเปอร์ออกซีคลอไรด์ 65.2% WG \n- เพนทิโอไพแรด 20%SC")
+                    
+                    HStack {
+                        Image(systemName: "lightbulb.fill")
+                            .foregroundColor(Color("FFC700"))
+                        Text("Tips: ผสมน้ำในอัตราส่วนตามที่ระบุไว้ใน ฉลากคำแนะนำ และฉีดพ่นทุก 7 วัน")
+                    }
+                    .frame(maxWidth: 300, minHeight: 47)
+                    .cornerRadius(10)
+                    .background(Color("FEFCCB"))
+                }
+            }
+            .font(.system(size: 14))
+            .padding(10)
+        }
+        .frame(maxWidth: 360)
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(Color("grey-light"), lineWidth: 1)
+        )
+    }
+}
+
+struct Instruction: View {
+    var body: some View {
+        HStack(alignment: .top) {
+            Group {
+                Image(systemName: "checkmark.shield")
+                    .foregroundColor(Color("A3B694"))
+                    .padding(.trailing, 8)
+                
+                VStack(alignment: .leading) {
+                    Text("แนวทางป้องกัน")
+                        .font(.system(size: 14))
+                        .bold()
+                        .foregroundColor(Color("dark-green"))
+                        .padding(.bottom, 4)
+                    
+                    Text("-ควรหมั่นตรวจแปลงปลูกอย่างสม่ำเสมอ \n- ตัดส่วนและเก็บเศษพืชที่เป็นโรค และใบที่ร่วงหล่นนำไปทำลาย นอกแปลงปลูก \n- ตัดแต่งกิ่งมีทรงพุ่มโปร่ง เพื่อให้ทุเรียนได้รับแสงแดด และอากาศถ่ายเทได้ดี \n- ไม่ควรใส่ปุ๋ยที่มีไนโตรเจนสูงสำหรับแปลงปลูกที่มีความชื้นสูงและมีการระบาดของโรคเป็นประจำ")
+                        .font(.system(size: 14))
+                }
+            }
+            .padding(10)
+        }
+        .frame(maxWidth: 360)
         .overlay(
             RoundedRectangle(cornerRadius: 10)
                 .stroke(Color("grey-light"), lineWidth: 1)
