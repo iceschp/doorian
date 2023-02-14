329a2a5c92fb56ce0e90e3676aacb8089ed30df0
iceschp <warunya.ks@gmail.com>
2023-02-14 15:53:40 +0700

    add result view

---
+++ doorian/Diagnose/ResultView.swift
@@ -0,0 +1,24 @@
+//
+//  ResultView.swift
+//  doorian
+//
+//  Created by Warunya on 14/2/2566 BE.
+//
+
+import SwiftUI
+
+struct ResultView: View {
+    var body: some View {
+        ZStack {
+            Image("durian_temp1")
+                .resizable()
+                .ignoresSafeArea()
+        }
+    }
+}
+
+struct ResultView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResultView()
+    }
+}
